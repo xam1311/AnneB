81012a427af643d63ce33fb09476e1922b0cf231
xam1311 <github@designmyco.de>
2016-03-13 20:04:04 +0100

    Modification arborescence

--- app/Plugin/TwitterCake/Controller/TwitterCakeAppController.php
+++ app/Plugin/TwitterCake/Controller/TwitterCakeAppController.php
@@ -1,4 +1,8 @@
 <?php
+
+require "vendor/autoload.php";
+
+use Abraham\TwitterOAuth\TwitterOAuth;
 // /app/Plugin/TwitterCake/Controller/TwitterCakeAppController.php:
 
 class TwitterCakeAppController extends AppController {
---
+++ app/Plugin/TwitterCake/View/Helper/TwitterCakeHelper.php
@@ -0,0 +1,9 @@
+<?php
+
+class TwitterCakeHelper extends AppHelper{
+
+
+
+
+  
+}
